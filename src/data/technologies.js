5a8993a05366824274c83711024862c9caa2278e
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-22 18:54:16 +0300

    initialised my mockdata

---
+++ src/data/technologies.js
@@ -0,0 +1,32 @@
+export const technologies = [
+    {
+        id: 1,
+        name: "Vue.js",
+        level: "Advanced",
+    },
+    {
+        id: 2,
+        name: "React.js",
+        level: "Intermediate",
+    },
+    {
+        id: 3,
+        name: "Node.js",
+        level: "Advanced",
+    },
+    {
+        id: 4,
+        name: "Express.js",
+        level: "Advanced",
+    },
+    {
+        id: 5,
+        name: "postgressql",
+        level: "Advanced",
+    },
+    {
+        id: 6,
+        name: "Docker",
+        level: "Advanced",
+    }
+]
