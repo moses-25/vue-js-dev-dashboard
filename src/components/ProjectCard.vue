25b79c5d7bce5cf3b504396ab1cdf450e89a00df
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-24 18:29:43 +0300

    Add ProjectCard component with favorite toggle

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(
+    {
+        project: Object
+    }
+);
+const emit = defineEmits(["favorite"]);
+</script>
+
+<template>
+    <div class="project-card">
+        <h3>{{ project.title }}</h3>
+
+        <p>{{ project.tech }}</p>
+       
+    </div>
+    <button 
+    @click="emit ('favourite', project.id)"
+    :class="{active: project.favorite }"
+    >
+    Favorite
+        </button>
+
+</template>
