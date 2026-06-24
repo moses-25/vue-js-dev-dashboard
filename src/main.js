4ee8b7436d3f05c22cc21a5dd93c1a2ef1d82e14
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-24 18:30:21 +0300

    Refactor main.js with router integration and comments

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "./Styles/App.css"
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+// import the main App components 
+import App from './App.vue';
+// import the vue application router
+import router from "./router";
+// or import router from "./router/index.js";
+
+//import the styles
+import './Styles/App.css';
+
+/*
+*create the vue application inside the HTML element with the id app and use the vue.js router
+*/
+
+createApp(App)
+    .use(router)
+    .mount('#app')
