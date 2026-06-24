2813f6d2c22125e32abd1899ac1b5415206e0689
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-24 18:29:36 +0300

    Add Navbar component with RouterLink navigation

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar">
+        <h2>Vue Dashboard</h2>
+
+        <div>
+        <RouterLink to="/"
+        >
+        Dashboard
+    </RouterLink>
+
+        <RouterLink 
+            to="/projects"
+            >
+            Projects
+        </RouterLink>
+        
+        </div>
+    </nav>
+</template>
